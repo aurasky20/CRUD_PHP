039ebb620e40d37bceb3613b1f21925b22f28c24
auras_ky20 <aurasasi20@gmail.com>
2024-11-19 12:50:31 +0700

    Menambah repo baru buat crud-php

--- delete_buku.php
+++ delete_buku.php
@@ -1,13 +1,4 @@
 <?php
-include "koneksi_oracle.php";
-
-$no_buku = "10";
-
-$sql=ociparse($con,"delete buku where nobuku='$no_buku'");
-ociexecute($sql);
-if (ocirowcount($sql)>0)
-	echo "Berhasil Delete Data";
-else
-	echo "Gagal Delete Data";	
+phpinfo();
 
 ?>
